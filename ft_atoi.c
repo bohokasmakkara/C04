a9d1e8369df17e65fa8b15e71067c91e65bab2a6
bohokasmakkara <162553789+bohokasmakkara@users.noreply.github.com>
2024-03-07 14:11:48 +0200

    Create ft_atoi.c
    • Write a function that converts the initial portion of the string pointed by str to its int representation
    • The string can start with an arbitray amount of white space (as determined by isspace(3))
    • The string can be followed by an arbitrary amount of + and - signs, - sign will change the sign of the int returned based on the number of - is odd or even.
    • Finally the string can be followed by any numbers of the base 10.
    • Your function should read the string until the string stop following the rules and return the number found until now.
    • You should not take care of overflow or underflow. result can be undefined in that case.

---
+++ ft_atoi.c
@@ -0,0 +1,24 @@
+int	ft_atoi(char *str)
+{
+	int	res;
+	int	sign;
+	int	i;
+
+	res = 0;
+	sign = 1;
+	i = 0;
+	while (((str[i] >= 9) && (str[i] <= 13)) || (str[i] == 32))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while ((str[i] >= '0') && (str[i] <= '9'))
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	return (sign * res);
+}
